bb2cd601edaa0cdfa55ec509d5112e6615c26fc9
Fuad <fuadabdaz99@gmail.com>
2023-03-18 13:13:37 -0700

    completed assignment

---
+++ index.js
@@ -0,0 +1,20 @@
+// Define the divide function expression
+const divide = function() {
+    return 2000 / 100;
+  }
+  
+  // Define the square arrow function
+  const square = (num) => {
+    return num * num;
+  }
+  
+  // Define the add arrow function
+  const add = (num1, num2) => {
+    return num1 + num2;
+  }
+  
+  // Test the functions
+  console.log(divide()); // Output: 20
+  console.log(square(5)); // Output: 25
+  console.log(add(2, 3)); // Output: 5
+  
